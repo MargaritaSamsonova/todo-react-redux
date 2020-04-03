ec207adc41a1ca62c3bb76f043f70bbf29bd9871
Margarita Samsonova <margaritasamsonova01@gmail.com>
2020-04-03 15:17:07 +0300

    Updates

---
+++ precache-manifest.948d13d86ecc42349022bb47c60abda7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac83a9e4fcdc404e4ef4cbdcfdc5e494",
+    "url": "/todo-react-redux/index.html"
+  },
+  {
+    "revision": "97fbe5d443ec72a01947",
+    "url": "/todo-react-redux/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9a188dae75a68addeadd",
+    "url": "/todo-react-redux/static/js/2.5ef55805.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-react-redux/static/js/2.5ef55805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97fbe5d443ec72a01947",
+    "url": "/todo-react-redux/static/js/main.b0106058.chunk.js"
+  },
+  {
+    "revision": "1cf92ff87c184100ea49",
+    "url": "/todo-react-redux/static/js/runtime-main.e36fae08.js"
+  }
+]);
